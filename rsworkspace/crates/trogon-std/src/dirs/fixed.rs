6828a4d8f1db90acb0690c38afea3ef1930e5422
Yordis Prieto <yordis.prieto@gmail.com>
2026-02-19 12:27:40 -0500

    feat(trogon-std): add dirs module for platform directory resolution (#7)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/trogon-std/src/dirs/fixed.rs
+++ rsworkspace/crates/trogon-std/src/dirs/fixed.rs
@@ -25,7 +25,9 @@ pub struct FixedDirs {
 #[cfg(any(test, feature = "test-support"))]
 impl FixedDirs {
     pub fn new() -> Self {
-        Self { dirs: HashMap::new() }
+        Self {
+            dirs: HashMap::new(),
+        }
     }
 
     pub fn set(&mut self, kind: DirKind, path: impl Into<PathBuf>) -> &mut Self {
@@ -145,7 +147,10 @@ mod tests {
         let mut dirs = FixedDirs::new();
         dirs.set(DirKind::State, "/var/state");
 
-        assert_eq!(get_state(&dirs, "myapp"), Some(PathBuf::from("/var/state/myapp")));
+        assert_eq!(
+            get_state(&dirs, "myapp"),
+            Some(PathBuf::from("/var/state/myapp"))
+        );
     }
 
     #[test]
