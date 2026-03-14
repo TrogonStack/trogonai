e8d64015622f0acfe78ce13717edeaa060eb5e41
Yordis Prieto <yordis.prieto@gmail.com>
2026-03-14 07:42:57 -0400

    feat(acp-nats): add Bridge infrastructure for session cancel and ready (#34)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/acp-nats/src/nats/parsed_client_subject.rs
+++
@@ -1,9 +0,0 @@
-use crate::session_id::AcpSessionId;
-
-use super::client_method::ClientMethod;
-
-#[derive(Debug)]
-pub struct ParsedClientSubject {
-    pub session_id: AcpSessionId,
-    pub method: ClientMethod,
-}
