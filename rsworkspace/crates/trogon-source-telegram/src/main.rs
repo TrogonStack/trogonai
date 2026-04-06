4bc4eb446cf6742db533ec180d13c9bada105509
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-06 06:11:12 +0100

    feat(trogon-source-telegram): add Telegram webhook source adapter (#102)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

---
+++ rsworkspace/crates/trogon-source-telegram/src/main.rs
@@ -0,0 +1,47 @@
+#[cfg(not(coverage))]
+use {
+    acp_telemetry::ServiceName, tracing::error, tracing::info, trogon_nats::connect,
+    trogon_nats::jetstream::NatsJetStreamClient, trogon_source_telegram::TelegramSourceConfig,
+    trogon_source_telegram::constants::DEFAULT_NATS_CONNECT_TIMEOUT, trogon_std::env::SystemEnv,
+    trogon_std::fs::SystemFs,
+};
+
+#[cfg(not(coverage))]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = TelegramSourceConfig::from_env(&SystemEnv);
+
+    acp_telemetry::init_logger(
+        ServiceName::TrogonSourceTelegram,
+        &config.subject_prefix,
+        &SystemEnv,
+        &SystemFs,
+    );
+
+    info!("Telegram webhook source starting");
+
+    let nats = connect(&config.nats, DEFAULT_NATS_CONNECT_TIMEOUT).await?;
+    let js = NatsJetStreamClient::new(async_nats::jetstream::new(nats));
+    let result = trogon_source_telegram::serve(js, config).await;
+
+    if let Err(ref e) = result {
+        error!(error = %e, "Telegram webhook source stopped with error");
+    } else {
+        info!("Telegram webhook source stopped");
+    }
+
+    acp_telemetry::shutdown_otel();
+
+    result.map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+}
+
+#[cfg(coverage)]
+fn main() {}
+
+#[cfg(all(coverage, test))]
+mod tests {
+    #[test]
+    fn coverage_stub() {
+        super::main();
+    }
+}
