e76f789968effb396868e6059a03884bc29f5437
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-12 13:37:47 -0400

    refactor(trogon-cron): tighten command handler boundaries
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/trogon-cron/src/commands/mod.rs
+++ rsworkspace/crates/trogon-cron/src/commands/mod.rs
@@ -14,15 +14,30 @@ pub async fn handle_job(
     action: cli::JobAction,
 ) -> Result<(), Box<dyn std::error::Error>> {
     match action {
-        cli::JobAction::List => list::run(&store).await.map_err(Into::into),
-        cli::JobAction::Get { id } => get::run(&store, &id).await.map_err(Into::into),
-        cli::JobAction::Add { file } => add::run(&store, &file).await.map_err(Into::into),
-        cli::JobAction::Remove { id } => remove::run(&store, &id).await.map_err(Into::into),
-        cli::JobAction::Enable { id } => set_state::run(&store, &id, JobEnabledState::Enabled)
+        cli::JobAction::List => list::run(&store, list::ListCommand)
             .await
             .map_err(Into::into),
-        cli::JobAction::Disable { id } => set_state::run(&store, &id, JobEnabledState::Disabled)
+        cli::JobAction::Get { id } => get::run(&store, get::GetCommand::try_from(id)?)
             .await
             .map_err(Into::into),
+        cli::JobAction::Add => {
+            let command = add::read_from_stdin()?;
+            add::run(&store, command).await.map_err(Into::into)
+        }
+        cli::JobAction::Remove { id } => remove::run(&store, remove::RemoveCommand::try_from(id)?)
+            .await
+            .map_err(Into::into),
+        cli::JobAction::Enable { id } => set_state::run(
+            &store,
+            set_state::SetStateCommand::new(id, JobEnabledState::Enabled)?,
+        )
+        .await
+        .map_err(Into::into),
+        cli::JobAction::Disable { id } => set_state::run(
+            &store,
+            set_state::SetStateCommand::new(id, JobEnabledState::Disabled)?,
+        )
+        .await
+        .map_err(Into::into),
     }
 }
