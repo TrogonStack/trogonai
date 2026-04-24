3dd2345171cd3dad6cf7bdf55dd23fa09caddc35
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-24 02:27:21 -0400

    refactor(cron): clarify command state module naming
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/trogon-cron/src/commands/mod.rs
+++ rsworkspace/crates/trogon-cron/src/commands/mod.rs
@@ -1,13 +1,13 @@
 mod add_job;
 pub mod domain;
-mod job_state;
 mod pause_job;
 mod remove_job;
 mod resume_job;
 mod snapshot;
+mod state;
 
 pub use add_job::{AddJobCommand, AddJobDecisionError};
-pub use job_state::JobState;
 pub use pause_job::{PauseJobCommand, PauseJobDecisionError};
 pub use remove_job::{RemoveJobCommand, RemoveJobDecisionError};
 pub use resume_job::{ResumeJobCommand, ResumeJobDecisionError};
+pub use state::JobState;
