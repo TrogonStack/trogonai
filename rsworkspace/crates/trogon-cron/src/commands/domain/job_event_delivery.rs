b4e1941e666440592ce0413c1a90e0de32423f9b
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-27 12:44:25 -0400

    Add proto conversion impls for domain types
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/trogon-cron/src/commands/domain/job_event_delivery.rs
+++ rsworkspace/crates/trogon-cron/src/commands/domain/job_event_delivery.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use trogon_cron_jobs_proto::v1;
+
+use crate::commands::proto::JobEventProtoError;
 
 use super::JobEventSamplingSource;
 
@@ -12,4 +15,42 @@ pub enum JobEventDelivery {
         #[serde(default, skip_serializing_if = "Option::is_none")]
         source: Option<JobEventSamplingSource>,
     },
+}
+
+impl From<&JobEventDelivery> for v1::JobDelivery {
+    fn from(value: &JobEventDelivery) -> Self {
+        let mut delivery = v1::JobDelivery::new();
+        match value {
+            JobEventDelivery::NatsEvent { route, ttl_sec, source } => {
+                let mut inner = v1::NatsEventDelivery::new();
+                inner.set_route(route.as_str());
+                if let Some(ttl_sec) = ttl_sec {
+                    inner.set_ttl_sec(*ttl_sec);
+                }
+                if let Some(source) = source {
+                    inner.set_source(v1::JobSamplingSource::from(source));
+                }
+                delivery.set_nats_event(inner);
+            }
+        }
+        delivery
+    }
+}
+
+impl TryFrom<v1::JobDelivery> for JobEventDelivery {
+    type Error = JobEventProtoError;
+
+    fn try_from(value: v1::JobDelivery) -> Result<Self, Self::Error> {
+        match value.kind() {
+            v1::job_delivery::KindOneof::NatsEvent(inner) => Ok(Self::NatsEvent {
+                route: inner.route().to_string(),
+                ttl_sec: inner.has_ttl_sec().then(|| inner.ttl_sec()),
+                source: inner
+                    .has_source()
+                    .then(|| inner.source().to_owned().try_into())
+                    .transpose()?,
+            }),
+            v1::job_delivery::KindOneof::not_set(_) | _ => Err(JobEventProtoError::MissingDeliveryKind),
+        }
+    }
 }
