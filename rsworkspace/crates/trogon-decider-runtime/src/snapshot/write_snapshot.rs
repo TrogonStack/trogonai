ff2a4da62787eb0b01b15ee8b46574d245640dcf
Yordis Prieto <yordis.prieto@gmail.com>
2026-05-28 20:53:32 -0400

    fix(snapshot): decouple snapshot identity from storage keys
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- rsworkspace/crates/trogon-decider-runtime/src/snapshot/write_snapshot.rs
+++ rsworkspace/crates/trogon-decider-runtime/src/snapshot/write_snapshot.rs
@@ -1,19 +1,19 @@
 use super::Snapshot;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct WriteSnapshotRequest<'a, SnapshotPayload, StreamId: ?Sized> {
-    pub stream_id: &'a StreamId,
+pub struct WriteSnapshotRequest<'a, SnapshotPayload, SnapshotId: ?Sized> {
+    pub snapshot_id: &'a SnapshotId,
     pub snapshot: Snapshot<SnapshotPayload>,
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct WriteSnapshotResponse;
 
-pub trait SnapshotWrite<SnapshotPayload, StreamId: ?Sized>: Send + Sync {
+pub trait SnapshotWrite<SnapshotPayload, SnapshotId: ?Sized>: Send + Sync {
     type Error: std::error::Error + Send + Sync + 'static;
 
     fn write_snapshot(
         &self,
-        request: WriteSnapshotRequest<'_, SnapshotPayload, StreamId>,
+        request: WriteSnapshotRequest<'_, SnapshotPayload, SnapshotId>,
     ) -> impl std::future::Future<Output = Result<WriteSnapshotResponse, Self::Error>> + Send;
 }
