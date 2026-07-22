fff36e0a9e52acf3c75b82d862a516314fdaf9f4
Yordis Prieto <yordis.prieto@gmail.com>
2026-07-22 14:19:33 -0400

    feat(wit): prevent protobuf and WIT schema drift
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

---
+++ goworkspace/internal/protowit/errors.go
@@ -0,0 +1,72 @@
+package protowit
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type unsupportedReason uint8
+
+const (
+	unsupportedParameter unsupportedReason = iota + 1
+	unsupportedPackage
+	unsupportedEnum
+	unsupportedOneofShape
+	unsupportedMap
+	unsupportedFieldKind
+	unsupportedMessageReference
+	unsupportedRecursiveMessage
+	unsupportedExtension
+	unsupportedService
+	unsupportedIdentifier
+	unsupportedNameCollision
+)
+
+type schemaError struct {
+	element protoreflect.FullName
+	reason  unsupportedReason
+	detail  string
+}
+
+func (e *schemaError) Error() string {
+	location := string(e.element)
+	if location == "" {
+		location = "request"
+	}
+	if e.detail == "" {
+		return fmt.Sprintf("%s: %s", location, e.reason)
+	}
+	return fmt.Sprintf("%s: %s: %s", location, e.reason, e.detail)
+}
+
+func (r unsupportedReason) String() string {
+	switch r {
+	case unsupportedParameter:
+		return "plugin parameters are not supported"
+	case unsupportedPackage:
+		return "protobuf package must contain a namespace and name"
+	case unsupportedEnum:
+		return "enums are not supported"
+	case unsupportedOneofShape:
+		return "oneof shape is not supported"
+	case unsupportedMap:
+		return "maps are not supported"
+	case unsupportedFieldKind:
+		return "field kind is not supported"
+	case unsupportedMessageReference:
+		return "message reference is not part of the generated WIT package"
+	case unsupportedRecursiveMessage:
+		return "recursive message value types are not supported"
+	case unsupportedExtension:
+		return "extensions are not supported"
+	case unsupportedService:
+		return "services are not supported"
+	case unsupportedIdentifier:
+		return "protobuf identifier cannot be represented in WIT"
+	case unsupportedNameCollision:
+		return "normalized WIT name collision"
+	default:
+		return "unsupported schema"
+	}
+}
